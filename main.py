74e6d27b6e258e2f97e5a73373a85e9bf51e0d7a
sprajtzero <sprajtzero@gmail.com>
2026-09-11 20:58:43 +0200

    refactor: add enums and game class

--- main.py
+++ main.py
@@ -1,32 +1,64 @@
 import random
-
-VALID_MOVES = ["rock", "paper", "scissors"]
-
-
-def get_move() -> str:
-    return random.choice(VALID_MOVES)
+from enum import StrEnum, auto
 
 
-def get_user_move() -> str:
-    while (move := input("Your move: ").lower()) not in VALID_MOVES:
-        print("Choose a valid move. Rock, paper or scissors")
-    return move
+class ValidMove(StrEnum):
+    ROCK = auto()
+    PAPER = auto()
+    SCISSORS = auto()
+
+
+class Outcome(StrEnum):
+    WIN = auto()
+    LOSS = auto()
+    DRAW = auto()
+
+
+class RPS:
+    def get_move(self) -> ValidMove:
+        return random.choice(list(ValidMove))
+
+    def get_user_move(self) -> ValidMove:
+        while (
+            (move := input("Your move: ").lower())
+            not in list(ValidMove)
+        ):
+            print("Choose a valid move. Rock, paper or scissors")
+        return ValidMove(move)
+
+    def determine_win(
+        self,
+        first: ValidMove,
+        second: ValidMove
+    ) -> Outcome:
+        if first == second:
+            return Outcome.DRAW
+        elif (
+            (first == ValidMove.ROCK and second == ValidMove.SCISSORS)
+            or 
+            (first == ValidMove.SCISSORS and second == ValidMove.PAPER)
+            or
+            (first == ValidMove.PAPER and second == ValidMove.ROCK)
+        ):
+            return Outcome.WIN
+        else:
+            return Outcome.LOSS
+    
+    def run(self) -> None:
+        computer_move = self.get_move()
+        user_move = self.get_user_move()
+        outcome = self.determine_win(user_move, computer_move)
+        if outcome == Outcome.WIN:
+            print("You win!!")
+        elif outcome == Outcome.LOSS:
+            print("You lose")
+        elif outcome == Outcome.DRAW:
+            print("Draw!")
 
 
 def main():
-    computer_move = get_move()
-    user_move = get_user_move()
-
-    if computer_move == user_move:
-        print("Draw!")
-    elif (
-        (computer_move == "rock" and user_move == "scissors")
-        or (computer_move == "scissors" and user_move == "paper")
-        or (computer_move == "paper" and user_move == "rock")
-    ):
-        print("You lose")
-    else:
-        print("You win!!")
+    rps = RPS()
+    rps.run()
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,14 +1,15 @@
 import unittest
 from unittest.mock import patch
 
-from main import get_move, get_user_move
+from main import RPS
 
 
 class TestCode(unittest.TestCase):
     def test_computer_choice_valid(self):
         valid = ["rock", "paper", "scissors"]
+        app = RPS()
 
-        actual = get_move()
+        actual = app.get_move()
 
         self.assertTrue(actual in valid)
 
@@ -16,7 +17,38 @@ class TestCode(unittest.TestCase):
     def test_user_input_must_bevalid(self, mock_input):
         valid = ["rock", "paper", "scissors"]
         mock_input.side_effect = ["", "wrong", " ", "paper", "wrong"]
+        app = RPS()
 
-        actual = get_user_move()
+        actual = app.get_user_move()
 
         self.assertTrue(actual in valid)
+
+    def test_outcome_win(self):
+        first = "rock"
+        second = "scissors"
+        app = RPS()
+        expected = "win"
+
+        actual = app.determine_win(first, second)
+        
+        self.assertEqual(expected, actual)
+
+    def test_outcome_lossn(self):
+        first = "rock"
+        second = "paper"
+        app = RPS()
+        expected = "loss"
+
+        actual = app.determine_win(first, second)
+        
+        self.assertEqual(expected, actual)
+
+    def test_outcome_draw(self):
+        first = "rock"
+        second = "rock"
+        app = RPS()
+        expected = "draw"
+
+        actual = app.determine_win(first, second)
+        
+        self.assertEqual(expected, actual)
